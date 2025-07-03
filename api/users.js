d17a6c01b5f36a0c9f75cdcc0303e373b83d5560
Miller Andres Ostos Castro <78831533+milkos12@users.noreply.github.com>
2025-07-03 13:33:59 -0500

    Update users.js

--- api/users.js
+++ api/users.js
@@ -10,10 +10,10 @@ export default async function handlerUsers(req, res) {
 
     let users = await kv.get('users');
 
-    if (!users) {
+    
         await kv.set('users', db.users);
         users = await kv.get('users');
-    }
+    
 
     const user = users.find((user) => {
         return user.id == parseInt(id);
